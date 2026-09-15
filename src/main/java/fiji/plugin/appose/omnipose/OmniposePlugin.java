e1875d4371386637dc007cc7882ec39b055425cd
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-09-15 14:51:03 +0200

    Keep the Python env open until the plugin is closed.
    Greatly accelerate preview and parameter tuning.

--- src/main/java/fiji/plugin/appose/omnipose/OmniposePlugin.java
+++ src/main/java/fiji/plugin/appose/omnipose/OmniposePlugin.java
@@ -1,19 +1,35 @@
 package fiji.plugin.appose.omnipose;
 
 import static fiji.plugin.appose.ApposeUtils.addROIs;
+import static fiji.plugin.appose.ApposeUtils.clearOutsideRoi;
+import static fiji.plugin.appose.ApposeUtils.getAxisInfo;
+import static fiji.plugin.appose.ApposeUtils.rawWraps;
+import static fiji.plugin.appose.ApposeUtils.transferCalibration;
 
 import java.awt.Color;
 
 import org.scijava.ui.config.fiji.ConfigFijiPluginPreviewable;
+import org.scijava.ui.config.visitors.gui.FrameBuilder.ConfigFrame;
 
 import fiji.plugin.appose.listeners.FijiApposeProgressListener;
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.Roi;
 import ij.plugin.frame.RoiManager;
+import net.imagej.ImgPlus;
+import net.imglib2.appose.util.AxisInfo;
+import net.imglib2.omnipose.OmniposeOutput;
 import net.imglib2.omnipose.OmniposeParameters;
+import net.imglib2.omnipose.OmniposeRunner;
 
 public class OmniposePlugin extends ConfigFijiPluginPreviewable< OmniposeConfig >
 {
+
+	private OmniposeRunner runner;
+
+	private String previousTorchVersion;
+
+	private FijiApposeProgressListener listener;
 
 	protected OmniposeParameters toParams( final OmniposeConfig config )
 	{
@@ -22,12 +38,13 @@ public class OmniposePlugin extends ConfigFijiPluginPreviewable< OmniposeConfig 
 
 		final OmniposeParameters params = OmniposeParameters.builder()
 				.model( isBuiltin ? config.builtinModel().getValue() : null )
-				.customModel( isBuiltin ? null :  config.customModel().getValue() )
+				.customModel( isBuiltin ? null : config.customModel().getValue() )
 				.diameter( config.diameter().getValue() )
 				.channels( config.channel().getValue(), 0 )
 				.minSize( config.minSize().getValue() )
 				.normalize( config.normalize().getValue() )
-				.resample( true ) // Must be true here, as we expect the output to have the same size as the input.
+				.resample( true ) // Must be true here, as we expect the output
+									// to have the same size as the input.
 				.maskThreshold( config.maskThreshold().getValue() )
 				.flowThreshold( config.flowThreshold().getValue() )
 				.tileOverlap( config.tileOverlap().getValue() )
@@ -50,17 +67,57 @@ public class OmniposePlugin extends ConfigFijiPluginPreviewable< OmniposeConfig 
 		return new OmniposeConfig( nChannels, pixelSize, units );
 	}
 
+	@SuppressWarnings( "unchecked" )
 	@Override
 	protected void process( final ImagePlus imp, final int tOrigin )
 	{
+		progress.clear();
+		final long startTime = System.currentTimeMillis();
+		// Store the roi in the source image for later.
+		final Roi roi = imp.getRoi();
+
 		try
 		{
 			// Convert config to Cellpose parameters.
 			final OmniposeParameters params = toParams( config );
 
+			// Create the runner if it doesn't exist or recreate it.
+			if ( runner == null || !params.torchVersion.equals( previousTorchVersion ) )
+			{
+				listener = new FijiApposeProgressListener( progress, config.getName() );
+				runner = OmniposeRunner.create( listener, params.torchVersion );
+				runner.init();
+				previousTorchVersion = params.torchVersion;
+			}
+
+			// Wrap input.
+			Roi initialRoi = imp.getRoi();
+			if ( initialRoi != null )
+				initialRoi = ( Roi ) initialRoi.clone();
+			@SuppressWarnings( "rawtypes" )
+			final ImgPlus input = rawWraps( imp );
+			final AxisInfo inputAxes = getAxisInfo( input );
+
 			// Exec.
-			final FijiApposeProgressListener listener = new FijiApposeProgressListener( progress, config.getName() );
-			final ImagePlus[] outputs = Omnipose.omnipose( imp, params, listener );
+			runner.setInput( input, inputAxes );
+			runner.run( params );
+			final OmniposeOutput< ? > oo = runner.getOutput();
+
+			final long endTime1 = System.currentTimeMillis();
+			progress.message( String.format( "Omnipose done in %.1f seconds. Postprocessing outputs...",
+					( endTime1 - startTime ) / 1000.0 ) );
+
+			// Clear outside of the ROI, if any.
+			clearOutsideRoi( oo.labels, initialRoi );
+			if ( oo.flows != null )
+				clearOutsideRoi( oo.flows, initialRoi );
+
+			// To ImagePlus.
+			final ImagePlus[] outputs = Omnipose.toImp( oo );
+
+			// Reposition the outputs.
+			for ( final ImagePlus out : outputs )
+				transferCalibration( imp, out, initialRoi );
 
 			// Unwrap the outputs and show them.
 			final ImagePlus labels = outputs[ 0 ];
@@ -82,5 +139,38 @@ public class OmniposePlugin extends ConfigFijiPluginPreviewable< OmniposeConfig 
 		{
 			IJ.handleException( e );
 		}
+		finally
+		{
+			// Restore the roi in the source image.
+			imp.setRoi( roi );
+			progress.clear();
+			final long endTime2 = System.currentTimeMillis();
+			progress.message( String.format( "Done in %.1f seconds.", ( endTime2 - startTime ) / 1000.0 ) );
+		}
+	}
+
+	/**
+	 * Adds a hook the UI, so that we close the {@link #runner} when the UI is
+	 * closed.
+	 */
+	@Override
+	protected ConfigFrame showUI()
+	{
+		final ConfigFrame ui = super.showUI();
+		ui.addWindowListener( new java.awt.event.WindowAdapter()
+		{
+			@Override
+			public void windowClosed( final java.awt.event.WindowEvent e )
+			{
+				if ( runner != null )
+				{
+					runner.close();
+					runner = null;
+					listener.close();
+					listener = null;
+				}
+			}
+		} );
+		return ui;
 	}
 }
